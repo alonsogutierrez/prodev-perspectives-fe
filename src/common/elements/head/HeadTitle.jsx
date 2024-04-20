31b24183824f0208902d9a25c0c88da50ff6659e
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2024-04-19 22:36:24 -0400

    feat(favicon): add real favicon

--- src/common/elements/head/HeadTitle.jsx
+++ src/common/elements/head/HeadTitle.jsx
@@ -1,32 +1,32 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const HeadTitle = ({ pageTitle }) => {
-  const title = 'ProDev Perspectives - Alonso G. Blog';
+  const title = "ProDev Perspectives - Alonso G. Blog";
   return (
     <Head>
       <title>{`${pageTitle} | ${title}`}</title>
-      <meta name='description' content={title} />
-      <meta property='og:title' content={`${pageTitle} | ${title}`} />
-      <meta name='robots' content='index,follow' />
-      <meta charSet='utf-8' />
-      <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+      <meta name="description" content={title} />
+      <meta property="og:title" content={`${pageTitle} | ${title}`} />
+      <meta name="robots" content="index,follow" />
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta
-        name='viewport'
-        content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
       <link
-        rel='icon'
-        type='image/x-icon'
+        rel="icon"
+        type="image/x-icon"
         href={`${
-          process.env.NODE_ENV === 'production'
-            ? process.env.NEXT_PUBLIC_BASEPATH ?? ''
-            : ''
-        }/favicon.ico`}
+          process.env.NODE_ENV === "production"
+            ? process.env.NEXT_PUBLIC_BASEPATH ?? ""
+            : ""
+        }/favicon_2024.ico`}
       />
       {/* Google Search Console */}
       <meta
-        name='google-site-verification'
-        content='Lu8zeIDoCYQidTR9ZCXXxCFu9zYde5BDKNX2u0GFgT0'
+        name="google-site-verification"
+        content="Lu8zeIDoCYQidTR9ZCXXxCFu9zYde5BDKNX2u0GFgT0"
       />
     </Head>
   );
