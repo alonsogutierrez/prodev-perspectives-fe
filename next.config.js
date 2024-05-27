2604b12790a2ce04993152bf118cded1b23a235f
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2024-05-26 23:58:42 -0400

    feat(img): add prodev s3 bukcet domain as valid domain

--- next.config.js
+++ next.config.js
@@ -2,17 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath:
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? process.env.NEXT_PUBLIC_BASEPATH
-      : '',
+      : "",
   env: {
     NEXT_PUBLIC_GA_ID: process.env.NEXT_PUBLIC_GA_ID,
     NEXT_GOOGLE_ADSENSE_ID: process.env.NEXT_GOOGLE_ADSENSE_ID,
   },
-  // images: {
-  //   loader: 'akamai',
-  //   path: process.env.NEXT_PUBLIC_URL,
-  // },
+  images: {
+    domains: ["prodevperspectives-images.s3.us-east-2.amazonaws.com"],
+  },
 };
 
 module.exports = nextConfig;
--- src/common/components/post/format/element/PostTagShare.jsx
+++ src/common/components/post/format/element/PostTagShare.jsx
@@ -1,26 +1,26 @@
-import Link from 'next/link';
-import { slugify } from '../../../../utils';
+import Link from "next/link";
+import { slugify } from "../../../../utils";
 
 const PostTagShare = ({ postTags }) => {
   return (
     <>
-      <div className='tagcloud'>
+      <div className="tagcloud">
         {postTags.tags.map((data, index) => (
           <Link href={`/tag/${slugify(data)}`} key={index}>
             <a>{data}</a>
           </Link>
         ))}
       </div>
-      <div className='social-share-block'>
-        <ul className='social-icon icon-rounded-transparent md-size'>
+      <div className="social-share-block">
+        <ul className="social-icon icon-rounded-transparent md-size">
           <li>
-            <a href='https://www.linkedin.com/in/alonso-guti%C3%A9rrez-b27370126'>
-              <i className='fab fa-linkedin-in' />
+            <a href="https://www.linkedin.com/in/alonso-guti%C3%A9rrez-b27370126">
+              <i className="fab fa-linkedin-in" />
             </a>
           </li>
           <li>
-            <a href='https://github.com/alonsogutierrez'>
-              <i className='fab fa-github' />
+            <a href="https://github.com/alonsogutierrez">
+              <i className="fab fa-github" />
             </a>
           </li>
         </ul>
