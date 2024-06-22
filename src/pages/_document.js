06f4bbf8b584bb82837a273ec373f857bdf3e74e
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2024-06-22 17:10:00 -0400

    feat(blog): add new styles to each blog post in lists

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,22 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 const Document = () => {
   return (
     <Html>
       <Head>
         <link
-          rel='stylesheet'
+          rel="stylesheet"
           href={`${
-            isProduction ? process.env.NEXT_PUBLIC_BASEPATH ?? '' : ''
+            isProduction ? process.env.NEXT_PUBLIC_BASEPATH ?? "" : ""
           }/css/font-awesome.css`}
         />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link
-          href='https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500&display=swap'
-          rel='stylesheet'
+          rel="stylesheet"
+          href={`${
+            isProduction ? process.env.NEXT_PUBLIC_BASEPATH ?? "" : ""
+          }/css/prodev-styles.css`}
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <body>
