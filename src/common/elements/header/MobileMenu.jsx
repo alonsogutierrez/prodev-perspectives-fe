d3501df389e8cb89cdfbb1ebddfb75ebc297e7e3
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2024-07-02 21:43:21 -0400

    feat(path): path finder project with more detailed titles

--- src/common/elements/header/MobileMenu.jsx
+++ src/common/elements/header/MobileMenu.jsx
@@ -1,58 +1,59 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import MenuData from '../../../data/mobilemenu/MenuData.json';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import MenuData from "../../../data/mobilemenu/MenuData.json";
 
 const MobileMenu = ({ menuShow, menuHide }) => {
   const MenuToggleHandler = (e) => {
     let selectElm = e.target.nextSibling;
-    if (!selectElm.classList.contains('open')) {
-      selectElm.classList.add('open');
-      e.target.classList.add('open');
+    if (!selectElm.classList.contains("open")) {
+      selectElm.classList.add("open");
+      e.target.classList.add("open");
     } else {
-      selectElm.classList.remove('open');
-      e.target.classList.remove('open');
+      selectElm.classList.remove("open");
+      e.target.classList.remove("open");
     }
   };
 
   return (
-    <div className={`popup-mobilemenu-area ${menuShow ? 'show' : ''}`}>
-      <div className='inner'>
-        <div className='mobile-menu-top'>
-          <div className='logo'>
-            <Link href='/'>
+    <div className={`popup-mobilemenu-area ${menuShow ? "show" : ""}`}>
+      <div className="inner">
+        <div className="mobile-menu-top">
+          <h2>test</h2>
+          <div className="logo">
+            <Link href="/">
               <Image
-                className='dark-logo'
+                className="dark-logo"
                 width={120}
                 height={120}
-                src='/images/logo/prodev-02.png'
-                alt='ProDev Perspectives logo'
+                src="/images/logo/prodev-02.png"
+                alt="ProDev Perspectives logo"
               />
             </Link>
           </div>
-          <div className='mobile-close' onClick={menuHide}>
-            <div className='icon'>
-              <i className='fal fa-times' />
+          <div className="mobile-close" onClick={menuHide}>
+            <div className="icon">
+              <i className="fal fa-times" />
             </div>
           </div>
         </div>
 
-        <ul className='mainmenu'>
+        <ul className="mainmenu">
           {MenuData.map((data, index) => (
             <li
-              className={`${data.submenu ? 'menu-item-has-children' : ''}`}
+              className={`${data.submenu ? "menu-item-has-children" : ""}`}
               key={index}
             >
               <a href={data.path} onClick={MenuToggleHandler}>
                 {data.label}
               </a>
               {data.submenu ? (
-                <ul className='axil-submenu'>
+                <ul className="axil-submenu">
                   {data.submenu.map((submenu, index) => (
                     <li key={index}>
                       <Link href={submenu.subpath}>
-                        <a className='hover-flip-item-wrapper'>
-                          <span className='hover-flip-item'>
+                        <a className="hover-flip-item-wrapper">
+                          <span className="hover-flip-item">
                             <span data-text={submenu.sublabel}>
                               {submenu.sublabel}
                             </span>
@@ -63,7 +64,7 @@ const MobileMenu = ({ menuShow, menuHide }) => {
                   ))}
                 </ul>
               ) : (
-                ''
+                ""
               )}
             </li>
           ))}
--- src/pages/path-finder-visualizer.js
+++ src/pages/path-finder-visualizer.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import HeadTitle from '../common/elements/head/HeadTitle';
-import SubHeader from '../common/elements/header/SubHeader';
+import HeadTitle from "../common/elements/head/HeadTitle";
+import SubHeader from "../common/elements/header/SubHeader";
 
-import Footer from '../common/elements/footer/Footer';
+import Footer from "../common/elements/footer/Footer";
 
-import PathFinder from '../common/components/path-finder-visualizer/PathFinder';
+import PathFinder from "../common/components/path-finder-visualizer/PathFinder";
 
 const PathFinderVisualizer = () => {
   return (
     <>
-      <HeadTitle pageTitle='Post Archive' />
-      <SubHeader pClass='header-light header-sticky header-with-shadow' />
-      <div className='axil-post-list-area axil-section-gap bg-color-white'>
-        <div className='container'>
-          <h3>Path Finder Visualizer</h3>
-          <PathFinder></PathFinder>
+      <HeadTitle pageTitle="Path Finder Visualizer" />
+      <SubHeader pClass="header-light header-sticky header-with-shadow" />
+      <div className="axil-post-list-area axil-section-gap bg-color-white">
+        <div className="container">
+          <h3>Path Finder Visualizer - Search Algorithms</h3>
+          <PathFinder />
         </div>
       </div>
       <Footer />
