3d4b0821128ad71dfa21cff666e87b9754bc44e8
alonsogutierrez <alonso.gutierrez@mail.udp.cl>
2025-04-05 11:43:14 -0300

    Add new portfolio project

--- src/common/components/path-finder-visualizer/home/PortfolioHome.js
+++ src/common/components/path-finder-visualizer/home/PortfolioHome.js
@@ -1,6 +1,59 @@
 import Link from "next/link";
 import Image from "next/image";
 
+const portfolioItems = [
+  {
+    href: "/path-finder-visualizer",
+    imgSrc: "/images/portfolio/path-finder-visualizer.png",
+    imgAlt: "Path finder visualizer",
+    imgHeight: 600,
+    imgWidth: 600,
+    category: "DSA Path Finder Visualizer",
+    title: "Path Finder Visualizer with Dijkstra - A* - DFS - BFS",
+    titleStyle: {
+      color: "#00aae6",
+      textShadow: "2px 2px 1px #000",
+      fontWeight: "bold",
+    },
+    containerClass: "post-grid post-grid-transparent", // Clase específica para el contenedor
+    rowClass: "col-xl-6 col-md-12 col-12 mt--30", // Clase específica para el primer div
+  },
+  {
+    href: "https://todoonadatattooart.com",
+    imgSrc:
+      "https://prodevperspectives-images.s3.us-east-2.amazonaws.com/images/medium/ecommerce.png",
+    imgAlt: "Todo o Nada Tattoo Art Ecommerce CL",
+    imgHeight: 300, // Tamaño más pequeño
+    imgWidth: 550, // Tamaño más pequeño
+    category: "Todo o Nada Tatto Art Ecommerce CL",
+    title: "Ecommerce Web CL - Microservices",
+    titleStyle: {
+      //color: "#333", // Letras no rojas
+      //textShadow: "1px 1px 2px #000",
+      //fontWeight: "normal", // Peso de fuente diferente
+    },
+    containerClass: "post-default image-rounded", // Clase específica para el contenedor
+    rowClass: "col-xl-3 col-md-6 col-12 mt--20", // Clase específica para el primer div
+  },
+  {
+    href: "https://karuncafe.com",
+    imgSrc:
+      "https://prodevperspectives-images.s3.us-east-2.amazonaws.com/images/medium/karuncafe.png",
+    imgAlt: "Karün Cafe website",
+    imgHeight: 220, // Tamaño más pequeño
+    imgWidth: 400, // Tamaño más pequeño
+    category: "Static website",
+    title: "Static website - SSR",
+    titleStyle: {
+      //color: "#333", // Letras no rojas
+      //textShadow: "1px 1px 2px #000",
+      //fontWeight: "normal", // Peso de fuente diferente
+    },
+    containerClass: "post-default image-rounded", // Clase específica para el contenedor
+    rowClass: "col-xl-3 col-md-6 col-12 mt--20", // Clase específica para el primer div
+  },
+];
+
 const PortfolioHome = () => {
   return (
     <>
@@ -18,30 +71,30 @@ const PortfolioHome = () => {
                 Visit our ProDev Perspectives&apos;s portfolio
               </h1>
             </div>
-            <div className="col-xl-6 col-md-12 col-12 mt--30">
-              <div className="content-block post-grid post-grid-transparent">
-                <div className="post-thumbnail">
-                  <Link href={`/path-finder-visualizer`}>
-                    <a>
-                      <Image
-                        src={"/images/portfolio/path-finder-visualizer.png"}
-                        alt={"Path finder visualizer"}
-                        height={600}
-                        width={600}
-                        priority={true}
-                      />
-                    </a>
-                  </Link>
-                </div>
-                <div className="post-grid-content">
+            {portfolioItems.map((item, index) => (
+              <div key={index} className={item.rowClass}>
+                <div className={`content-block ${item.containerClass}`}>
+                  <div className="post-thumbnail">
+                    <Link href={item.href}>
+                      <a>
+                        <Image
+                          src={item.imgSrc}
+                          alt={item.imgAlt}
+                          height={item.imgHeight}
+                          width={item.imgWidth}
+                          priority={true}
+                        />
+                      </a>
+                    </Link>
+                  </div>
                   <div className="post-content">
                     <div className="post-cat">
                       <div className="post-cat-list">
-                        <Link href={`/path-finder-visualizer`}>
+                        <Link href={item.href}>
                           <a className="hover-flip-item-wrapper">
                             <span className="hover-flip-item">
-                              <span data-text="dsa-and-alg">
-                                DSA Path Finder Visualizer
+                              <span data-text={item.category}>
+                                {item.category}
                               </span>
                             </span>
                           </a>
@@ -49,69 +102,14 @@ const PortfolioHome = () => {
                       </div>
                     </div>
                     <h3 className="title">
-                      <Link href={`/path-finder-visualizer`}>
-                        <a
-                          style={{
-                            color: "#00aae6",
-                            textShadow: "2px 2px 1px #000",
-                            fontWeight: "bold",
-                            backgroundColor: "#fff",
-                          }}
-                        >
-                          Path Finder Visualizer with Dijkstra - A* - DFS - BFS
-                        </a>
+                      <Link href={item.href}>
+                        <a style={item.titleStyle || {}}>{item.title}</a>
                       </Link>
                     </h3>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="col-xl-6 col-lg-6 col-md-12 col-md-6 col-12">
-              <div className="row">
-                {/**
-                 * TODO: Add Path Finder Visualizer here
-                 */}
-                <div className="col-lg-6 col-md-6 col-sm-6 col-12 mt--30">
-                  <div className="content-block post-default image-rounded">
-                    <div className="post-thumbnail">
-                      <Link href="https://todoonadatattooart.com">
-                        <a>
-                          <Image
-                            src={
-                              "https://prodevperspectives-images.s3.us-east-2.amazonaws.com/images/medium/ecommerce.png"
-                            }
-                            alt={"Todo o Nada Tattoo Art Ecommerce CL"}
-                            height={200}
-                            width={100}
-                            priority={true}
-                          />
-                        </a>
-                      </Link>
-                    </div>
-                    <div className="post-content">
-                      <div className="post-cat">
-                        <div className="post-cat-list">
-                          <Link href="https://todoonadatattooart.com">
-                            <a className="hover-flip-item-wrapper">
-                              <span className="hover-flip-item">
-                                <span data-text="path-finder-visualizer-search-alg">
-                                  Todo o Nada Tatto Art Ecommerce CL
-                                </span>
-                              </span>
-                            </a>
-                          </Link>
-                        </div>
-                      </div>
-                      <h5 className="title">
-                        <Link href="https://todoonadatattooart.com">
-                          <a>Ecommerce Web CL - Microservices</a>
-                        </Link>
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
